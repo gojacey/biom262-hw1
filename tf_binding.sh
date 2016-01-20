7b180df3122b8c6c496610df99dd90d4d1f32b62
Michael Baughn <mbaughn@gmail.com>
2016-01-20 15:11:19 -0800

    Removed #yourcodehere comments, and adjusted formatting

--- tf_binding.sh
+++ tf_binding.sh
@@ -1,7 +1,6 @@
 bash --out exercise1
 cd ~/home/ucsd-train16/code/biom262-2016/weeks/week01/data
 cat tf.bed | grep "NFKB" | sed -n '1,702'p > tf.nfkb.bed
-# YOUR CODE HERE
 wc -l tf.nfkb.bed
 echo '--- First 10 lines ---'
 head tf.nfkb.bed
@@ -15,7 +14,6 @@ print (exercise1)
 #Start of exercise2
 bash --out exercise2
 awk '$3 == "transcript" {print $0}' gencode.v19.annotation.chr22.gtf > gencode.v19.annotation.chr22.transcript.gtf
-# YOUR CODE HERE
 wc -l gencode.v19.annotation.chr22.transcript.gtf
 echo '--- First 10 lines ---'
 head gencode.v19.annotation.chr22.transcript.gtf
@@ -27,11 +25,9 @@ print (exercise2)
 #End of exercise2
 
 #Start of exercise3
-
 bash --out exercise3
 module load biotools
 bedtools flank -i gencode.v19.annotation.chr22.transcript.gtf -g hg19.genome -l 2000 -r 0 -s > gencode.v19.annotation.chr22.transcript.promoter.gtf
-# YOUR CODE HERE
 wc -l gencode.v19.annotation.chr22.transcript.promoter.gtf
 echo '--- First 10 lines ---'
 head gencode.v19.annotation.chr22.transcript.promoter.gtf
