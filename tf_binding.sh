299f9997c3b18e2ca4425110ac4da6e67cbddf47
Michael Baughn <mbaughn@gmail.com>
2016-01-20 15:07:36 -0800

    Added code for parts 4 & 5

--- tf_binding.sh
+++ tf_binding.sh
@@ -41,4 +41,34 @@ echo '--- Last 10 lines ---'
 tail gencode.v19.annotation.chr22.transcript.promoter.gtf
 #End of exercise3
 
+#Start of Exercise 4: Use bedtools intersect to overlap TFs with promoters
+%%bash --out exercise4
+module load biotools
+bedtools intersect -a gencode.v19.annotation.chr22.transcript.promoter.gtf -b tf.nfkb.bed > gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
+wc -l gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
+echo '--- First 10 lines ---'
+head gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
+echo '--- Random 10 lines ---'
+awk -v seed=908 'BEGIN{srand(seed);}{ if (rand() < 0.5 ) {print $0}}' gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf | head
+echo '--- Last 10 lines ---'
+tail gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
+#End of Exercise 4
+
+#Start of Exercise 5: Use bedtools getfasta to extract sequences
+%%bash --out exercise5
+module load biotools
+bedtools getfasta -fi GRCh37.p13.chr22.fa -bed gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf -s -fo gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
+#yes the strand matters!
+wc -l gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
+echo '--- First 10 lines ---'
+head gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
+echo '--- Random 10 lines ---'
+awk -v seed=908 'BEGIN{srand(seed);}{ if (rand() < 0.5 ) {print $0}}' gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta | head
+echo '--- Last 10 lines ---'
+tail gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
+#End of Exercise 5
+
+
+
+
 
