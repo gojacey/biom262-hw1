e2c5ff210c59d55fbbe986a29bb38b5c51f1cdf0
UCSD Training ADmin <ucsd-train16@tscc-login2.sdsc.edu>
2016-01-20 23:27:25 -0800

    Please enter the commit message for your changes. Lines starting

--- tf_binding.sh
+++ tf_binding.sh
@@ -1,4 +1,4 @@
-#!/bin/csh
+
 #PBS -q hotel
 #PBS -N Biom262Hw1_GroupAssignment
 #PBS -l nodes=1:ppn=1
@@ -6,7 +6,7 @@
 #PBS -V
 #PBS -M mbaughn@ucsd.edu,jil655@ucsd.edu
 #PBS -m abe
-#PBS -A ucsd-train03
+#PBS -A ucsd-train16
 cd /oasis/tscc/scratch/ucsd-train16/week1hw
 
 #Exercise1
@@ -36,9 +36,9 @@ bedtools getfasta -fi GRCh37.p13.chr22.fa -bed gencode.v19.annotation.chr22.tran
 
 #Submitter Instruction 6 messages:
 
-#person2 (Michael) adds:
-echo "Hello I am a message in standard error (stderr)" > &2
-#explainer from instructions:  ">&2 outputs to "secondary" aka "error" output)"
-
-#Person1 Jijun adds:
 echo "Hello I am a message in standard out (stdout)"
+#person2 (Michael) adds:
+
+echo "Hello I am a message in standard error (stderr)" >&2
+
+#explainer from instructions:  ">&2 outputs to "secondary" aka "error" output)"
