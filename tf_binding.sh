e8ba7a460c99788d5c137b05289efcc4ae3db8a1
Michael Baughn <mbaughn@gmail.com>
2016-01-20 18:53:53 -0800

    Removed Bash calls and code to check the output in a Jupyter notebook

--- tf_binding.sh
+++ tf_binding.sh
@@ -11,70 +11,29 @@
 #PBS -A ucsd-train03
 cd /oasis/tscc/home/ucsd-train03/group_project/biom262-hw1/data
 
-bash --out exercise1
+#Exercise1
 cd ~/home/ucsd-train16/code/biom262-2016/weeks/week01/data
 cat tf.bed | grep "NFKB" | sed -n '1,702'p > tf.nfkb.bed
-wc -l tf.nfkb.bed
-echo '--- First 10 lines ---'
-head tf.nfkb.bed
-echo '--- Random 10 lines ---'
-awk -v seed=907 'BEGIN{srand(seed);}{print $0}'  tf.nfkb.bed | head
-echo '--- Last 10 lines ---'
-tail tf.nfkb.bed
-print (exercise1)
 #End of exercise1
 
 #Start of exercise2
-bash --out exercise2
 awk '$3 == "transcript" {print $0}' gencode.v19.annotation.chr22.gtf > gencode.v19.annotation.chr22.transcript.gtf
-wc -l gencode.v19.annotation.chr22.transcript.gtf
-echo '--- First 10 lines ---'
-head gencode.v19.annotation.chr22.transcript.gtf
-echo '--- Random 10 lines ---'
-awk -v seed=907 'BEGIN{srand(seed);}{print $0}' gencode.v19.annotation.chr22.transcript.gtf | head
-echo '--- Last 10 lines ---'
-tail gencode.v19.annotation.chr22.transcript.gtf
-print (exercise2)
 #End of exercise2
 
 #Start of exercise3
-bash --out exercise3
 module load biotools
 bedtools flank -i gencode.v19.annotation.chr22.transcript.gtf -g hg19.genome -l 2000 -r 0 -s > gencode.v19.annotation.chr22.transcript.promoter.gtf
-wc -l gencode.v19.annotation.chr22.transcript.promoter.gtf
-echo '--- First 10 lines ---'
-head gencode.v19.annotation.chr22.transcript.promoter.gtf
-echo '--- Random 10 lines ---'
-awk -v seed=907 'BEGIN{srand(seed);}{print rand()" "$0}' gencode.v19.annotation.chr22.transcript.promoter.gtf | head
-echo '--- Last 10 lines ---'
-tail gencode.v19.annotation.chr22.transcript.promoter.gtf
 #End of exercise3
 
 #Start of Exercise 4: Use bedtools intersect to overlap TFs with promoters
-bash --out exercise4
 module load biotools
 bedtools intersect -a gencode.v19.annotation.chr22.transcript.promoter.gtf -b tf.nfkb.bed > gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
-wc -l gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
-echo '--- First 10 lines ---'
-head gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
-echo '--- Random 10 lines ---'
-awk -v seed=908 'BEGIN{srand(seed);}{ if (rand() < 0.5 ) {print $0}}' gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf | head
-echo '--- Last 10 lines ---'
-tail gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
 #End of Exercise 4
 
 #Start of Exercise 5: Use bedtools getfasta to extract sequences
-bash --out exercise5
 module load biotools
 bedtools getfasta -fi GRCh37.p13.chr22.fa -bed gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf -s -fo gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
 #yes the strand matters!
-wc -l gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
-echo '--- First 10 lines ---'
-head gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
-echo '--- Random 10 lines ---'
-awk -v seed=908 'BEGIN{srand(seed);}{ if (rand() < 0.5 ) {print $0}}' gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta | head
-echo '--- Last 10 lines ---'
-tail gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
 #End of Exercise 5
 
 
